53faa6a3a6bce6166c948aa0f91da761fff7a22a
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-01-27 10:39:22 +0100

    add optimize for snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,6 +1,17 @@
 // Snowpack Configuration File
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
+// snowpack.config.js
+// Example: Using Snowpack's built-in bundling support
+
+module.exports = {
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2018',
+  },
+}
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
