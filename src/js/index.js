457e450808cc6bcb51c715af2cdd016f5b4d61ed
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-01-31 17:27:14 +0100

    Jsform (#17)
    * add counting option for one form
    
    * add js for all the form inputs and some styling
    
    * add js form in storybook and update other storybook files

--- src/js/index.js
+++ src/js/index.js
@@ -111,3 +111,44 @@ cards.forEach(card => {
     answer.classList.toggle('hidden')
   })
 })
+
+const forms = document.querySelectorAll('[data-js="formInput"')
+
+forms.forEach(formInput => {
+  const textarea = formInput.querySelector('[data-js="textarea"]')
+  const counter = formInput.querySelector('[data-js="counter"]')
+
+  textarea.addEventListener('input', () => {
+    const textLength = textarea.value.length
+    counter.innerHTML =
+      textarea.maxLength - textLength + '/' + textarea.maxLength
+  })
+})
+
+/* WORKS for one input area
+const textarea = document.querySelector('[data-js="formInput"]')
+const counter = document.querySelector('[data-js="counter"]')
+
+textarea.addEventListener('input', () => {
+  const textLength = textarea.value.length
+  counter.textContent = textarea.maxLength - textLength + '/100'
+  console.log(counter.textContent)
+})
+
+
+/* ANOTHER WAY WITH IF
+const formInput = getElement('formInput')
+
+formInput.addEventListener('input', event => {
+const target = event.currentTarget;
+const maxLength = target.getAttribute('maxlenght');
+const currentLength = target.value.length;
+
+if (currentLength >= maxLength) {
+  return console.log ("max reached");
+}
+
+console.log(`${maxLength - currentLength + " chars left"});
+})
+})
+*/
